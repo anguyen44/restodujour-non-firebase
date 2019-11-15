f2d1ca2d0f79768c1c249b7f5bdc31573ca1299b
hanaezaim <zaimhanae9@gmail.com>
2019-11-15 15:49:06 +0100

    Données JSON

---
+++ data.js
@@ -0,0 +1,38 @@
+export default data = [
+    {
+        id: 1,
+        image: 'https://www.fivelittlechefs.com/wp-content/uploads/2016/02/stpatricks-soft-pretzel.jpg',
+        Nom_resto: 'Chez SOAN',
+        Prix: 12.5,
+        adresse_resto:"1 Place de la Résistance, Blois, France ",
+        Entrees:"Rillons de porc",
+        plats:"Sot l'y laisse de dinde Gratin de corgettes",
+        desserts:"Tarte aux pommes et mirbelles",
+        Img_resto:""
+        
+    },
+    {
+        id: 2,
+        image: 'https://www.fivelittlechefs.com/wp-content/uploads/2012/02/strawberry-rocky-road-recipe.jpg',
+        Nom_resto:"Hôtel d'Anglettere",
+        Prix:22.00,
+        adresse_resto:"9 Rue du Guichet, Saint-Calais, France",
+        Entrees:"Gaspacho Concombre/menthe",
+        plats:"Escalope de volaille à la crème de Trôo",
+        desserts:"Café Gourmand",
+        Img_resto:""
+        
+    },
+    {
+        id: 3,
+        image: 'https://www.fivelittlechefs.com/wp-content/uploads/2016/02/valentine-coconut-macaroons.jpg',
+        Nom_resto: 'Chez Fatima',
+        Prix: 14.9,
+        Entrees: 'Salade',
+        plats: "couscous-Marocain",
+        adresse_resto:"10 rue de marche, Bourges, France",
+        desserts: "Tarte aux pommes",
+        Img_resto:""
+       
+    }
+]
